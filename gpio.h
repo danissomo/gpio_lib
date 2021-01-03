8a074cc48581fc2870288b9694544d992a65115b
Daniil <dan.2k64@gmail.com>
2021-01-03 15:03:10 +0300

    defined pins for omega2

--- gpio.h
+++ gpio.h
@@ -116,4 +116,26 @@ const int pinouts[] = {
 
 #ifdef OMEGA2
 
+#define PIN_11 11
+#define PIN_3   3
+#define PIN_2   2
+#define PIN_17 17
+#define PIN_16 16
+#define PIN_15 15
+#define PIN_46 46
+#define PIN_45 45
+#define PIN_9   9
+#define PIN_8   8
+#define PIN_7   7
+#define PIN_6   6
+#define PIN_1   1
+#define PIN_0   0
+#define PIN_13 13
+#define PIN_12 12
+#define PIN_38 38
+#define PIN_18 18
+#define PIN_19 19
+#define PIN_4   4
+#define PIN_5   5
+
 #endif
---
+++ omega2.h
@@ -0,0 +1,1 @@
+#define OMEGA2
